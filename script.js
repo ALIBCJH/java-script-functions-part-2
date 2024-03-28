9b977238ea49acb8bf09f87d95861d8a6c04de35
simonjuma_230 <simon.ombom22@students.dkut.ac.ke>
2024-03-28 22:36:12 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// program to print the text
+// declaring a function
+function greet(name) {
+  console.log("Hello " + name + ":)");
+}
+
+// variable name can be different
+let name = prompt("Enter a name: ");
+
+// calling function
+greet(name);
+
+//program that add 2 numbers
+function add(a, b){
+  console.log(a+b);
+}
+add(3,4);
+add(2,9);
+
+function add(a, b){
+  return a+b;
+}
+let number1 = parseFloat(prompt("Enter first number: "));
+let number2 = parseFloat(prompt("Enter second number"));
+  let result = add(number1, number2);
+  
+  cconsole.log("Tge sum is " + results);
+  
+//Program finding the square of a number
+let x = function (num) {
+  return num * num;
+};
+console.log(x(4));
+
+let y = x(3);
+console.log(y);
